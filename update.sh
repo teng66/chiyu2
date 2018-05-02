f98b920397da41190e54b2d7c008d5e43ef86b5b
teng66 <38450845+teng66@users.noreply.github.com>
2018-05-02 12:44:06 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ echo "准备开始安装"
 read -p "回车后开始安装"
 echo "请输入你的内网ip" 
 read -p "内网ip： " ip
-cp /root/pubgradar/restart.sh /root/restart.sh
+cp /root/chiyu2/restart.sh /root/restart.sh
 chmod +x restart.sh
 wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
 chmod +x shadowsocks-all.sh
@@ -28,13 +28,12 @@ cd libpcap-1.8.1
 make
 make install
 
-git clone https://github.com/794959818/pubgradar.git
-cd pubgradar/
+git clone https://github.com/teng66/chiyu2.git
+cd chiyu2/
 npm i
 npm i -g pino
 npm install -g forever
 forever start index.js sniff eth0 $ip | pino
 
-echo "搭建完成"
-echo "并不是一个人在战斗，加入我们6698530@qq.com"
-echo "你能加上你的QQ，但加不上我的心"
+echo "搭建完成 QQ - 473688870"
+
